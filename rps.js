a7c4829b47a4311e9c1dd1fd0422c475eec9dd2d
Jake Hofmann <Jfshofmann@gmail.com>
2022-07-11 22:59:22 -0400

    add UI

--- rps.js
+++ rps.js
@@ -1,8 +1,13 @@
 
 
 //Have computer randomly select a choice between rock, paper, and scissors
+let playerWins = 0 ;
+let computerWins = 0;
+let ties = 0;
 
 const rpsArray = ["Rock", "Paper", "Scissors"];
+const buttons = document.querySelectorAll('button');
+const resultSection = document.getElementById('roundLog')
 
 function computerPlay() {
     
@@ -12,57 +17,55 @@ function computerPlay() {
 
 }
 
+function resetGame() {
+    playerWins = 0;
+    computerWins = 0;
+    window.location.reload();
+}
 //Play round of Rock, Paper, Scissors
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
     //format input consistently
     
-
-    const formatPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-    //console.log(formatPlayerSelection);
-
-    //Determine a winner
-
-    if (rpsArray.includes(formatPlayerSelection)){
-        //console.log("You entered valid input.");
-        if(formatPlayerSelection === 'Rock' && computerSelection === 'Scissors' || formatPlayerSelection === 'Paper' && computerSelection === 'Rock' || formatPlayerSelection === 'Scissors' && computerSelection === 'Paper'){
-            return "You Win! " + String(formatPlayerSelection) + " beats " + String(computerSelection);
-        } else if( formatPlayerSelection === computerSelection) {
-            return "Tie!";
-        }
-        else { return "You lose! " + String(computerSelection) + " beats " + String(formatPlayerSelection); }
-    } else { alert("Improper Input!"); }
-    
-}
-
-//Play a game of Rock Paper Scissors
-function game() {
-    for (let i = 0; i < 5; i++){
-        let playerSelection = String(prompt("Rock, Paper, Scissors?"));
         let computerSelection = computerPlay();
-        let result = playRound(playerSelection, computerSelection)
-        console.log(result);
-        if (result.includes("Win")) {
-            playerWins++;
-            //console.log("Playerwins:"+playerWins);
-        } else if (result.includes("lose")) {
-            computerWins++;
-            //console.log("Computerwins:" + computerWins)
-        } else {ties++};
-         
-    }
-    if (computerWins > playerWins) {
-        console.log("You lost the game!");
-    } 
-    else if (playerWins > computerWins) {
-        console.log("You won the game!");
-    } else {console.log("It's a tie!")};
+        
+        const formatPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+        
+
+        //Determine a winner
+
+        if (rpsArray.includes(formatPlayerSelection)){
+            if(formatPlayerSelection === 'Rock' && computerSelection === 'Scissors' || formatPlayerSelection === 'Paper' && computerSelection === 'Rock' || formatPlayerSelection === 'Scissors' && computerSelection === 'Paper'){
+                playerWins += 1;
+                if(playerWins === 5){
+                    alert("YOU WIN THE WHOLE GAME!");
+                    resetGame();
+                }
+                else {return "You Win! " + String(formatPlayerSelection) + " beats " + String(computerSelection);}
+            } else if( formatPlayerSelection === computerSelection) {
+                ties =+ 1;
+                return "Tie!";
+            }
+            else {
+                computerWins += 1;
+                if(computerWins === 5){
+                    alert("YOU LOSE THE WHOLE GAME!");
+                    resetGame();
+                } 
+                return "You lose! " + String(computerSelection) + " beats " + String(formatPlayerSelection); }
+        } else { 
+            alert("Improper Input!"); }
+
 }
 
 
 
-let playerWins = 0 ;
-let computerWins = 0;
-let ties = 0;
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        resultSection.innerHTML += playRound(button.id) + "<br />";
+        document.getElementById("plyr").textContent = playerWins;
+        document.getElementById("cmp").textContent = computerWins;
+    });
+});
 
-game();
+
